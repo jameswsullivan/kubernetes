8badba74c2c31d698758b112bfe1034da119db43
James Sullivan <81947235+jameswsullivan@users.noreply.github.com>
2025-03-21 16:52:44 -0500

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -1,6 +1,4 @@
-# Count number of pods:
-kubectl get pods --all-namespaces -o jsonpath='{.items[*].spec.containers[*].name}' | wc -w
-kubectl get pods --all-namespaces -o jsonpath="{range .items[*]}{range .status.containerStatuses[?(@.state.running)]}{.name}{'\n'}{end}{end}" | wc -l
+##### PODS ##### 
 
 # List pods:
 kubectl get pods
@@ -12,32 +10,37 @@ kubectl get pods --all-namespaces -o=custom-columns=NAME:.metadata.name,NODE:.sp
 kubectl get pods --all-namespaces -o jsonpath="{.items[*].metadata.name}"
 kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].name}"
 kubectl get pods --all-namespaces -o jsonpath="{.items[*].metadata.name}" | wc -w
-
-# List namespaces:
-kubectl get namespaces
-kubectl get namespaces --output=jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.metadata.annotations.field\.cattle\.io/projectId}{"\n"}{end}'
-kubectl get namespaces --selector=field.cattle.io/projectId=<project-id> --output=jsonpath='{.items[*].metadata.name}{"\n"}'
+kubectl run <pod-name> --image=ubuntu:latest --namespace=<namespace-name> --command -- /bin/sh -c "tail -f /dev/null"
+kubectl delete pod <pod-name> -n <namespace-name>
 
 # List running containers:
 kubectl get pods --all-namespaces -o jsonpath="{range .items[*]}{.metadata.name}:{' '}{range .status.containerStatuses[?(@.state.running)]}{' '}{end}{'\n'}{end}" | wc -l
 kubectl get pods --all-namespaces -o jsonpath="{range .items[*]}{.metadata.name}:{' '}{range .status.containerStatuses[?(@.state.running)]}{' '}{end}{'\n'}{end}" | wc -w
 
-# Create/delete namespace, deploy, restart/delete pods:
+# Count number of pods:
+kubectl get pods --all-namespaces -o jsonpath='{.items[*].spec.containers[*].name}' | wc -w
+kubectl get pods --all-namespaces -o jsonpath="{range .items[*]}{range .status.containerStatuses[?(@.state.running)]}{.name}{'\n'}{end}{end}" | wc -l
+
+##### NAMESPACES ##### 
+
+# List namespaces:
+kubectl get namespaces
+kubectl get namespaces --output=jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.metadata.annotations.field\.cattle\.io/projectId}{"\n"}{end}'
+kubectl get namespaces --selector=field.cattle.io/projectId=<project-id> --output=jsonpath='{.items[*].metadata.name}{"\n"}'
 kubectl create namespace <namespace-name>
 kubectl annotate namespace mynamespace field.cattle.io/projectId=<project-id-in-Rancher>
 kubectl annotate namespace mynamespace field.cattle.io/creatorId=<user-id-in-Rancher>
-kubectl run <pod-name> --image=ubuntu:latest --namespace=<namespace-name> --command -- /bin/sh -c "tail -f /dev/null"
-kubectl delete pod <pod-name> -n <namespace-name>
+kubectl get namespace <namespace-name> -o jsonpath='{.metadata.annotations}' --namespace=<namespace-name>
 kubectl delete namespace <namespace-name>
 
 # Get annotations:
-kubectl get namespace <namespace-name> -o jsonpath='{.metadata.annotations}' --namespace=<namespace-name>
-
 for ns in $(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}'); do
   annotations=$(kubectl get namespace $ns -o jsonpath='{.metadata.annotations}' --namespace=$ns)
   echo "Annotations for namespace $ns:"
   echo "$annotations"
 done
+
+##### DEPLOYMENT ##### 
 
 # Deployment:
 kubectl create deployment <deployment-name> --image=<image> --replicas=2 --namespace=<namespace-name>
@@ -49,17 +52,26 @@ kubectl expose deployment <deployment-name> --name=<service-name> --port=80 --ta
 kubectl get services --namespace=<namespace-name>
 kubectl delete service <service-name> --namespace=<namespace-name>
 kubectl delete deployment <deployment-name> --namespace=<namespace-name>
+
 # Force delete a Service
 kubectl delete service <service-name> --namespace=<namespace-name> --force --grace-period=0
+
 # Force delete a Deployment
 kubectl delete deployment <deployment-name> --namespace=<namespace-name> --force --grace-period=0
 kubectl delete pod <pod-name> --namespace <namespace>
+
 # Restart deployment and check deployment status
 kubectl rollout restart deployment <deployment-name> --namespace <namespace>
 kubectl rollout status deployment <deployment-name> --namespace <namespace>
+
 # Change deployment's image
 kubectl set image deployment <deployment-name> <deployment-name>=<image-tag> -n <namespace>
 
+# Describe:
+kubectl describe persistentvolume <persistent-volume-name>
+kubectl describe persistentvolumeclaim <persistent-volume-claim-name> -n <namespace>
+
+##### CLUSTER #####
 
 # Check the Kubernetes Context
 kubectl config current-context
